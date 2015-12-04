83795684600f6407d5ee30bab071c0d625e8db56
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2015-12-04 01:07:43 +0100

    Misc tests and corrections

--- logic_analyzer/logic_analyzer.v
+++ logic_analyzer/logic_analyzer.v
@@ -2,6 +2,7 @@
 // Basado en los tutoriales de Juan Gonzalez ( https://github.com/Obijuan/open-fpga-verilog-tutorial )
 
 module program(input clk, input [7:0] logic_input, input FSDO, input FSCTS, output FSDI, output FSCLK, output LED);
+
 wire clk;
 wire FSDO;
 wire FSCTS;
@@ -10,26 +11,32 @@ wire FSCLK;
 
 wire [7:0] logic_input;
 
-wire [10:0] logic_input_registered;
 
-assign logic_input_registered[0] = 1; // FSDI=1 (idle state)
-assign logic_input_registered[1] = 0;
-assign logic_input_registered[9:2] = logic_input;
-assign logic_input_registered[10] = 1; // Destination bit (1=portB)
+reg send = 1;
+
+wire [10:0] output_frame;
+
+assign output_frame[0] = 1; // FSDI=1 (idle state)
+assign output_frame[1] = 0;
+assign output_frame[9:2] = logic_input;
+assign output_frame[10] = 1; // Destination bit (1=portB)
 
 reg [3:0] current_bit = 0;
 
 
 assign FSCLK = clk;
-assign LED = FSDI;
+assign LED = send;
 
+reg count_msb_delayed = 0;
 
-always @(negedge clk)
-    if ((FSCTS || current_bit > 0) && current_bit < 10) current_bit <= current_bit + 1;
-    else current_bit <= 0;
+always @(posedge clk) begin
+    if (((send & FSCTS) | current_bit > 0) & current_bit < 10)
+        current_bit <= current_bit + 1;
+     else
+        current_bit <= 0;
+    FSDI <= output_frame[current_bit];
+end
 
-always @(posedge clk)
-    FSDI <= logic_input_registered[current_bit];
 
 endmodule
 

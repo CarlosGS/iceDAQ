ec859a80cd8718710a9e5e564e924cbe32a34296
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2015-12-05 11:42:36 +0100

    PLL is working! Thanks to Clifford Wolf

--- pll_example/pll_example.v
+++ pll_example/pll_example.v
@@ -1,8 +1,9 @@
-// Based on code by Clifford Wolf ( https://github.com/cliffordwolf/icestorm/blob/master/icefuzz/tests/sb_pll40_core.v )
+// Using a PLL to make an LED blink at ~1.072Hz
+// Thanks to Clifford Wolf for his explanation on PLL usage (https://github.com/cliffordwolf/yosys/issues/107#issuecomment-162163626)
 
 module program(
-    input   clk,
-    output  LED
+    input   clk, // set_io clk 21
+    output  LED  // set_io LED 99
 );
 
     wire clk;
@@ -20,21 +21,29 @@ module program(
         .SHIFTREG_DIV_MODE(1'b0),
         .FDA_FEEDBACK(4'b0000),
         .FDA_RELATIVE(4'b0000),
-        .DIVR(4'b1111),
-        .DIVF(7'b0000000),
-        .DIVQ(3'b000),
+        .DIVR(4'b0000),
+        .DIVF(7'b0000010), // frecuency multiplier = 2+1 = 3
+        .DIVQ(3'b010),
         .FILTER_RANGE(3'b001),
     ) uut (
         .REFERENCECLK   (clk),
-        .PLLOUTCORE     (clk_new),
+        .PLLOUTCORE     (clk_new), // output frequency = 3 * input frequency
         .BYPASS         (BYPASS),
         .RESETB         (RESETB)
         //.LOCK (LOCK )
     );
 
-    assign LED = clk_new;
+    //assign LED = clk_new;
     assign BYPASS = 0;
-    assign RESETB = 0;
+    assign RESETB = 1;
+
+    // Prescaler from https://github.com/Obijuan/open-fpga-verilog-tutorial/wiki/Cap%C3%ADtulo-5%3A-Prescaler-de-N-bits
+    parameter N = 25;
+    reg [N-1:0] count = 0;
+    assign LED = count[N-1]; // MSB to output
+    always @(posedge(clk_new)) begin
+      count <= count + 1;
+    end
 
 endmodule
 
